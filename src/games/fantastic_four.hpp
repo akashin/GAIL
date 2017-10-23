fe6bfceaaed8b2941726fc54800b072a742d4746
akashin <kashin.andrej@gmail.com>
2017-10-23 22:44:46 +0100

    Implement FantasticFour simulator.

--- src/games/fantastic_four.hpp
+++ src/games/fantastic_four.hpp
@@ -15,12 +15,25 @@ namespace fantastic_four {
 
 const int H = 7;
 const int W = 8;
+using Field = std::array<std::array<int8_t, W>, H>;
+
+const std::array<int, 4> dRow = {1, 0, 1, -1};
+const std::array<int, 4> dCol = {0, 1, 1, 1};
 
 const int NO_PLAYER = 0;
+const int FIRST_PLAYER = 1;
+const int SECOND_PLAYER = 2;
+const int DRAW = 3;
 
 struct PlayerState {
+  PlayerState() = default;
+
+  PlayerState(int winner, int player_id, const Field& field)
+      : winner(winner), player_id(player_id), field(field) {}
+
+  int winner = NO_PLAYER;
   int player_id = NO_PLAYER;
-  std::array<std::array<int8_t, W>, H> field{};
+  Field field{};
 };
 
 struct PlayerAction {
@@ -62,6 +75,101 @@ private:
   std::istream& state_input_stream;
   std::ostream& action_output_stream;
 
+};
+
+class Simulator {
+public:
+  Simulator() = default;
+
+  explicit Simulator(const Field& field)
+      : field(field) {}
+
+  void makeAction(int8_t player_id, const PlayerAction& action) {
+    validateAction(player_id, action);
+
+    int row = -1;
+    while (row + 1 < H && field[row][action.column] == NO_PLAYER) {
+      ++row;
+    }
+    if (row == -1) {
+      throw std::logic_error("No free space left on column: " + std::to_string(action.column));
+    }
+
+    field[row][action.column] = player_id;
+    ++turn;
+    checkWinner();
+
+    if (winner != NO_PLAYER) {
+      expected_player_id = NO_PLAYER;
+    } else if (expected_player_id == FIRST_PLAYER) {
+      expected_player_id = SECOND_PLAYER;
+    } else if (expected_player_id == SECOND_PLAYER) {
+      expected_player_id = FIRST_PLAYER;
+    }
+  }
+
+  // TODO(akashin): Should this return PlayerState? Maybe only field and winner.
+  PlayerState getState(int player_id) {
+    return PlayerState(winner, player_id, field);
+  }
+
+private:
+  void validateAction(int player_id, const PlayerAction& action) {
+    if (player_id != expected_player_id) {
+      throw std::logic_error("Wrong player id, expected: " + std::to_string(expected_player_id) +
+                             ", found: " + std::to_string(player_id) + ".");
+    }
+
+    if (action.column < 0 or action.column >= W) {
+      throw std::logic_error("Wrong action column: " + std::to_string(action.column));
+    }
+  }
+
+  bool isValid(int row, int column) {
+    return (row >= 0 && column >= 0 && row < H && column < W);
+  }
+
+  void checkWinner() {
+    for (int row = 0; row < H; ++row) {
+      for (int col = 0; col < W; ++col) {
+        for (int d = 0; d < dRow.size(); ++d) {
+          if (field[row][col] == NO_PLAYER) {
+            continue;
+          }
+
+          bool covered = true;
+          for (int k = 0; k < 3; ++k) {
+            int nRow = row + k * dRow[d];
+            int nCol = col + k * dCol[d];
+
+            if (!isValid(nRow, nCol)) {
+              covered = false;
+              break;
+            }
+
+            if (field[nRow][nCol] != field[row][col]) {
+              covered = false;
+              break;
+            }
+          }
+
+          if (covered) {
+            winner = field[row][col];
+            return;
+          }
+        }
+      }
+    }
+
+    if (turn == H * W) {
+      winner = DRAW;
+    }
+  }
+
+  int turn = 0;
+  int winner = NO_PLAYER;
+  int expected_player_id = FIRST_PLAYER;
+  Field field{};
 };
 
 }; // namespace fantastic_four
