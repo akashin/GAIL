4cecfb416861e7767c5115138953f2e7bc2e2250
akashin <kashin.andrej@gmail.com>
2017-10-24 22:24:30 +0100

    Implement TreeSearchPlayer for FantasticFour.
    This commit implements the player that is based on tree search and state scoring function. This is one of the classes of opponents, that should be supported nicely by the library. In this commit we also add a basic match play between tree search players.

--- src/games/fantastic_four/fantastic_four.cpp
+++ src/games/fantastic_four/fantastic_four.cpp
@@ -4,15 +4,38 @@
 
 #include "fantastic_four.hpp"
 #include "../../core/player.hpp"
+#include "../../core/match.hpp"
 #include "random_player.hpp"
+#include "tree_search_player.hpp"
 
 #include <iostream>
 
 using namespace gail::fantastic_four;
 
-int main() {
+void playStreamGame() {
   StreamClient client(std::cin, std::cout);
   RandomPlayer player;
   client.makeAction(player.takeAction(client.getState()));
+}
+
+void playMatchGame() {
+  Simulator simulator;
+  SimulatorClient first_client(FIRST_PLAYER, simulator);
+  SimulatorClient second_client(SECOND_PLAYER, simulator);
+
+  TreeSearchPlayer first_player(FIRST_PLAYER);
+  TreeSearchPlayer second_player(SECOND_PLAYER);
+
+  auto scores = gail::playMatch<PlayerState, PlayerAction>({&first_client, &second_client},
+                                                           {&first_player, &second_player});
+  for (int i = 0; i < scores.size(); ++i) {
+    std::cout << "Score for player " << i << " is " << scores[i] << std::endl;
+  }
+
+  std::cout << simulator.getState().field << std::endl;
+}
+
+int main() {
+  playMatchGame();
   return 0;
 }
--- src/games/fantastic_four/fantastic_four.hpp
+++ src/games/fantastic_four/fantastic_four.hpp
@@ -17,6 +17,8 @@ const int H = 7;
 const int W = 8;
 using Field = std::array<std::array<int8_t, W>, H>;
 
+const int WIN_LENGTH = 4;
+
 const std::array<int, 4> dRow = {1, 0, 1, -1};
 const std::array<int, 4> dCol = {0, 1, 1, 1};
 
@@ -24,6 +26,16 @@ const int NO_PLAYER = 0;
 const int FIRST_PLAYER = 1;
 const int SECOND_PLAYER = 2;
 const int DRAW = 3;
+
+std::ostream &operator<<(std::ostream &os, const Field &field) {
+  for (int row = 0; row < H; ++row) {
+    for (int col = 0; col < W; ++col) {
+      os << int(field[row][col]) << " ";
+    }
+    os << std::endl;
+  }
+  return os;
+}
 
 }; // namespace fantastic_four
 }; // namespace gail
