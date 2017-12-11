0352f073b2122e5f3da8888f9a0b9f707b24347c
akashin <kashin.andrej@gmail.com>
2017-12-11 21:18:54 +0300

    Add entropy regularizer for knapsack.

--- src/games/knapsack/knapsack.cpp
+++ src/games/knapsack/knapsack.cpp
@@ -3,11 +3,14 @@
 //
 
 #include <unordered_set>
+#include <set>
 #include "clients.hpp"
 #include "players.hpp"
 #include "../../core/match.hpp"
 
 using namespace gail::knapsack;
+
+const double EPS = 1e-7;
 
 State generateState(int itemCount, int capacity) {
   State state;
@@ -34,13 +37,14 @@ float clamp(float x, float lo, float hi) {
 
 void solve_with_evolution(const State& state) {
   int populationSize = 500;
-  int iterationCount = 1000;
+  int iterationCount = 10000;
   float stepSize = 0.000005;
   int maxScore = -1000000000;
 
   EvolutionParameters parameters = generateEvolutionParameters(state);
   for (int iteration = 0; iteration < iterationCount; ++iteration) {
     std::vector<std::pair<Action, int>> populationWithScore;
+    float meanScore = 0;
     for (int i = 0; i < populationSize; ++i) {
       OneStateClient client(state);
       Action action(parameters.sample());
@@ -49,8 +53,10 @@ void solve_with_evolution(const State& state) {
       auto results = gail::playMatch<State, Action>({&client}, {&player});
       int score = results.scores.back();
       maxScore = std::max(maxScore, score);
+      meanScore += score;
       populationWithScore.emplace_back(action, score);
     }
+    meanScore /= populationSize;
 
     std::vector<float> gradient(parameters.probabilities.size(), 0.0);
     auto& probabilities = parameters.probabilities;
@@ -72,15 +78,22 @@ void solve_with_evolution(const State& state) {
     }
 
     double gradNorm = 0;
+    double entropy = 0;
     for (int j = 0; j < parameters.probabilities.size(); ++j) {
       gradNorm += gradient[j] * gradient[j];
       probabilities[j] += gradient[j] * stepSize;
-      probabilities[j] = clamp(probabilities[j], 0, 1);
+      probabilities[j] = clamp(probabilities[j], 0 + EPS, 1 - EPS);
+      entropy += -(probabilities[j] * log(probabilities[j]))
+                 -(1 - probabilities[j]) * log(1 - probabilities[j]);
     }
+    entropy /= parameters.probabilities.size();
     gradNorm = sqrt(gradNorm);
-    if (iteration % (iterationCount / 20) == 0) {
+    if (iteration % (iterationCount / 100) == 0) {
       std::cerr << "Evolution score at iteration " << iteration << ": " << maxScore
-                << ", gradient norm: " << gradNorm << std::endl;
+                << ", gradient norm: " << gradNorm
+                << ", entropy: " << entropy
+//                << ", mean score: " << meanScore
+                << std::endl;
     }
   }
 
@@ -88,7 +101,7 @@ void solve_with_evolution(const State& state) {
 }
 
 int main() {
-  State state = generateState(100, 1000);
+  State state = generateState(100, 5000);
   solve_greedily(state);
   solve_with_evolution(state);
 }
